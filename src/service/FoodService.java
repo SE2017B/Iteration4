39f130ae5e5651644e4bf1ee5e3cfc8e9b56443a
Tvpatterson <tvpatterson@wpi.edu>
2017-11-10 18:47:47 -0500

    Initialize Arraylist + add setters

--- src/service/FoodService.java
+++ src/service/FoodService.java
@@ -1,39 +1,43 @@
 package service;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class FoodService extends Service{
     private String foodType;
-    private String type;
-    List<Staff> personel;
 
     public FoodService (){
         this.type = "FoodService";
+        personnel = new ArrayList<>();
     }
 
     public FoodService (String foodType){
         this.foodType = foodType;
         this.type = "FoodService";
+        personnel = new ArrayList<>();
     }
 
     public void assignPerson(Staff person){
-        this.personel.add(person);
+        this.personnel.add(person);
     }
 
-    public void assignPeople(List<Staff> people){
-        for(Staff person: people)
-            this.personel.add(person);
-    }
-
-    public String getFoodType(){
-        return this.foodType;
+    public void assignPeople(ArrayList<Staff> people){
+        this.personnel.addAll(people);
     }
 
     public String getType(){
         return this.type;
     }
+    public ArrayList<Staff> getPersonnel(){
+        return this.personnel;
+    }
+    public String getFoodType(){
+        return this.foodType;
+    }
 
-    public List<Staff> getPersonel(){
-        return this.personel;
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setFoodType(String foodType){
+        this.foodType = foodType;
     }
 }
