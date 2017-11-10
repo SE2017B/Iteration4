caac56299f6d07d74271297f0268fcc67e4a218b
Tvpatterson <tvpatterson@wpi.edu>
2017-11-10 18:38:49 -0500

    Service Abstract

--- src/service/TranslatorService.java
+++ src/service/TranslatorService.java
@@ -1,29 +1,50 @@
 package service;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class TranslatorService extends Service{
     private String language;
     private int endTime;
-    private String type;
-    List<Staff> personel;
 
     public TranslatorService (){
         this.type = "TranslatorService";
+        this.personnel = new ArrayList<>();
     }
 
     public TranslatorService(String language, int endTime){
         this.language = language;
         this.endTime = endTime;
         this.type = "TranslatorService";
+        this.personnel = new ArrayList<>();
     }
 
     public void assignPerson(Staff person){
-        this.personel.add(person);
+        this.personnel.add(person);
     }
 
-    public void assignPeople(List<Staff> people){
-        for(Staff person: people)
-            this.personel.add(person);
+    public void assignPeople(ArrayList<Staff> people){
+        this.personnel.addAll(people);
+    }
+    public String getType(){
+        return this.type;
+    }
+    public ArrayList<Staff> getPersonnel(){
+        return this.personnel;
+    }
+    public String getLanguage(){
+        return this.language;
+    }
+    public int getEndTime(){
+        return this.endTime;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setLanguage(String language){
+        this.language = language;
+    }
+    public void setEndTime(int endTime){
+        this.endTime = endTime;
     }
 }
