78a7e36fd31c8ee80d29afa6fd303fca32d811a3
Tvpatterson <tvpatterson@wpi.edu>
2017-11-10 18:35:37 -0500

    Staff Test

---
+++ Test/TestServices/StaffTest.java
@@ -0,0 +1,27 @@
+package TestServices;
+
+import org.junit.Before;
+import org.junit.Test;
+import service.Staff;
+import static org.junit.Assert.*;
+
+public class StaffTest{
+
+    private Staff doctorBob;
+
+    public StaffTest(){}
+
+    @Before
+    public void initialize() {
+        doctorBob = new Staff("bob", "bobby", "Doctor", "Bobby Bob", 1);
+    }
+
+    @Test
+    public void testGetters(){
+        assertEquals(doctorBob.getUsername(), "bob");
+        assertEquals(doctorBob.getPassword(), "bobby");
+        assertEquals(doctorBob.getJobTitle(), "Doctor");
+        assertEquals(doctorBob.getFullName(), "Bobby Bob");
+        assertEquals(doctorBob.getID(), 1);
+    }
+}
