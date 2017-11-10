23e3b2bddef0595df0eec363ef1b0c6f2afa4afd
Tvpatterson <tvpatterson@wpi.edu>
2017-11-10 18:49:22 -0500

    Initialize Arraylist + add setters

--- src/service/TransportService.java
+++ src/service/TransportService.java
@@ -1,29 +1,44 @@
 package service;
 
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class TransportService extends Service{
     private String transportationType;
 
-    private String type;
-    List<Staff> personel;
-
     public TransportService (){
         this.type = "TransportationService";
+        this.personnel = new ArrayList<>();
     }
 
     public TransportService(String type){
         this.transportationType = type;
         this.type = "TransportationService";
+        this.personnel = new ArrayList<>();
     }
 
     public void assignPerson(Staff person){
-        this.personel.add(person);
+        this.personnel.add(person);
     }
 
-    public void assignPeople(List<Staff> people){
-        for(Staff person: people)
-            this.personel.add(person);
+    public void assignPeople(ArrayList<Staff> people){
+        this.personnel.addAll(people);
+    }
+
+    public String getType(){
+        return this.type;
+    }
+    public ArrayList<Staff> getPersonnel(){
+        return this.personnel;
+    }
+    public String getTransportationType(){
+        return this.transportationType;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setTransportationType(String transportationType){
+        this.transportationType = transportationType;
     }
 }
