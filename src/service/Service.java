d9d8a3577604c2b37b6a4aea5dfa648ade0f86fc
Tvpatterson <tvpatterson@wpi.edu>
2017-11-10 18:38:03 -0500

    Service Abstract

--- src/service/Service.java
+++ src/service/Service.java
@@ -1,15 +1,13 @@
 package service;
 
 import java.util.ArrayList;
-import java.util.List;
 
 abstract class Service{
     protected String type;
-    protected  List<Staff> personel;
-
-    abstract String getType();
-    abstract ArrayList<Staff> getPersonel();
+    protected ArrayList<Staff> personnel;
 
     abstract void assignPerson(Staff person); //Are these actually void? Just fixing errors -Travis
-    abstract void assignPeople(List<Staff> people);
+    abstract void assignPeople(ArrayList<Staff> people);
+    abstract String getType();
+    abstract ArrayList<Staff> getPersonnel();
 }
