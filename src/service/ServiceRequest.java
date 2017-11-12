00703056bb7d12c67ae3030a5829a549be4a877b
Tvpatterson <tvpatterson@wpi.edu>
2017-11-12 14:24:26 -0500

    Updated ServiceRequest class

--- src/service/ServiceRequest.java
+++ src/service/ServiceRequest.java
@@ -1,14 +1,33 @@
 package service;
 
-import java.util.ArrayList;
-import java.util.Map;
+import a_star.Node;
 
 public class ServiceRequest{
-        private ArrayList<Service> services;
+    private Service requestType;
+    private int requestID;
 
 
-        public ServiceRequest(String serviceReq){
 
-        }
+    public ServiceRequest(Service requestType, int requestID){
+        this.requestType = requestType;
+        this.requestID = requestID;
+    }
 
+    public void requestService(Service service, Node where){
+
+    }
+
+    public Service getRequestType(){
+        return this.requestType;
+    }
+    public int getRequestID(){
+        return this.requestID;
+    }
+
+    public void setRequestType(Service requestType){
+        this.requestType = requestType;
+    }
+    public void setRequestID(int requestID){
+        this.requestID = requestID;
+    }
 }
