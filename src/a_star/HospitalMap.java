76991c1605339f494c94ba019da9d04c60153185
Nicholas Fajardo <nafajardo@wpi.edu>
2017-11-12 13:15:36 -0500

    Deleted uneccesary files temporarily, cleaned up syntax

--- src/a_star/HospitalMap.java
+++ src/a_star/HospitalMap.java
@@ -1,5 +1,9 @@
 package a_star;
 
+import java.util.Dictionary;
+import java.util.List;
+import java.util.ArrayList;
+
 public class HospitalMap{
     private Dictionary<String, Node> map;
     private List<Node> frontier;
@@ -14,6 +18,7 @@ public class HospitalMap{
     public List<Node> findPath(Node start, Node end){
         //TODO
         //Also determine heuristic..should be manhattan but we can also use euclydian...up to the team
+        return (new ArrayList<Node>());
     }
 
     public void setDefault(Node defaultNode){
@@ -21,11 +26,11 @@ public class HospitalMap{
         //also ask what this does. Default = kiosk location?
     }
 
-    public void getDistance(Node start, Node end){
+    public double getDistance(Node start, Node end){
         //Gonna assume this is euclydian
-        xDeltaSquared = Math.pow((end.getX()-start.getX()), 2)
-        YDeltaSquared = Math.pow((end.getY()-start.getY()), 2)
-        distance = Math.sqrt(xDeltaSquared + yDeltaSquared)
+        double xDeltaSquared = Math.pow((end.getX()-start.getX()), 2);
+        double yDeltaSquared = Math.pow((end.getY()-start.getY()), 2);
+        double distance = Math.sqrt(xDeltaSquared + yDeltaSquared);
         return distance;
     }
 }
--- src/kioskEngine/KioskEngine.java
+++
@@ -1,7 +0,0 @@
-package kioskEngine;
-
-public class KioskEngine{
-    private Dictionary<String, Staff> staff; // the string field is the username
-    private HospitalMap map;
-    private services;
-}
