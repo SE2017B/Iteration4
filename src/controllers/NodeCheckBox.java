5bf9a7936690a077c6bee0b1b156893c306e8a83
Travis Norris <TravisYNorris@gmail.com>
2017-11-14 15:29:57 -0500

    Centering Node Check Boxes and automatically placing them

--- src/controllers/NodeCheckBox.java
+++ src/controllers/NodeCheckBox.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import a_star.Node;
+import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 
 public class NodeCheckBox extends CheckBox {
@@ -8,9 +9,13 @@ public class NodeCheckBox extends CheckBox {
 
     public void setNode(Node node) {
         this.node = node;
+        this.setLayoutX(node.getX()-10);
+        this.setLayoutY(node.getY()-10);
+        this.setAccessibleHelp(node.getName());
     }
 
     public Node getNode() {
         return node;
     }
+
 }
