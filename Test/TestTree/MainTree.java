81d9a85f24e1d6b5ef443b816b366b414e19cb20
Oluchukwu Okafor <ojokafor@wpi.edu>
2017-11-14 21:00:53 -0500

    Main Tests

---
+++ Test/TestTree/MainTree.java
@@ -0,0 +1,20 @@
+package TestTree;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class MainTree {
+    //Load objects from database
+
+    //maybe Load Object from CSV
+
+    //Object Nodes can access sibling Node
+
+    //Object can not access non sibling nodes
+
+    //Path finding test
+
+
+}
