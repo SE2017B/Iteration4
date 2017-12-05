b23ee8ab49c393483bbe60d3e890a826a885d19a
Erika Snow <essnow@wpi.edu>
2017-12-05 18:27:32 -0500

    encapsulated the shake transition and sped up the animation

--- src/ui/ShakeTransition.java
+++ src/ui/ShakeTransition.java
@@ -2,8 +2,6 @@ package ui;
 
 import javafx.animation.TranslateTransition;
 import javafx.scene.control.Control;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.TextField;
 import javafx.util.Duration;
 
 public class ShakeTransition {
@@ -14,21 +12,11 @@ public class ShakeTransition {
     }
 
     public void shake(Control c){
-        t = new TranslateTransition(Duration.millis(250), c);
-        t.setByX(25f);
+        t = new TranslateTransition(Duration.millis(100), c);
+        t.setByX(15f);
         t.setCycleCount(4);
         t.setAutoReverse(true);
         t.setDelay(Duration.millis(350));
         t.playFromStart();
     }
-
-    /*
-    public void shakeTextField(TextField m){
-        t.setByX(25f);
-        t.setCycleCount(4);
-        t.setAutoReverse(true);
-        t.setDelay(Duration.millis(350));
-        t.playFromStart();
-    }
-    */
 }
