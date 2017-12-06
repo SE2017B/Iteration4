4de5b491a407c79ce0f230e33b9cd5eb51b1f6ad
Travis Norris <TravisYNorris@gmail.com>
2017-12-06 03:35:03 -0500

    Resizing about and help pages

--- src/controllers/FeedbackController.java
+++ src/controllers/FeedbackController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.skins.JFXSliderSkin;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -27,7 +28,7 @@ public class FeedbackController implements ControllableScreen{
     private JFXSlider starSlider;
 
     @FXML
-    private Label HelpDescription;
+    private JFXTextArea HelpDescription;
 
     @FXML
     private Label HelpTitle;
@@ -48,13 +49,13 @@ public class FeedbackController implements ControllableScreen{
         populateLists();
         this.HelpTitle.setText("Main Menu");
         this.HelpDescription.setText("Here is the main screen for Brigham and Women's Hospital kiosk application that " +
-                "\nincludes several features. Along the top are buttons that access staff login (login), " +
-                "\na feedback/help/about screen (question mark), a filter for showing helpful icons on the map (filter), " +
-                "\na tool to find the nearest type of location from the kiosk location (Find Nearest), " +
-                "\nand a button that brings the user to a screen to find directions from one place to another. " +
-                "\nAlong the right side is a zoom feature, and the map can also be moved by scrolling with a " +
-                "\nmouse or clicking and dragging. Along the bottom of the map includes an emergency button (emergency) " +
-                "\nthat shows the nearest exit along with buttons to switch between floors of the hospital.");
+                "includes several features. Along the top are buttons that access staff login (login), " +
+                "a feedback/help/about screen (question mark), a filter for showing helpful icons on the map (filter), " +
+                "a tool to find the nearest type of location from the kiosk location (Find Nearest), " +
+                "and a button that brings the user to a screen to find directions from one place to another. " +
+                "Along the right side is a zoom feature, and the map can also be moved by scrolling with a " +
+                "mouse or clicking and dragging. Along the bottom of the map includes an emergency button (emergency) " +
+                "that shows the nearest exit along with buttons to switch between floors of the hospital.");
         this.setImageFromList(false);
     }
 
@@ -116,61 +117,61 @@ public class FeedbackController implements ControllableScreen{
     }
     private void populateLists(){
         CustomPair main = new CustomPair("Main Menu", "Here is the main screen for Brigham and Women's Hospital kiosk application that " +
-                "\nincludes several features. Along the top are buttons that access staff login (login), " +
-                "\na feedback/help/about screen (question mark), a filter for showing helpful icons on the map (filter), " +
-                "\na tool to find the nearest type of location from the kiosk location (Find Nearest), " +
-                "\nand a button that brings the user to a screen to find directions from one place to another. " +
-                "\nAlong the right side is a zoom feature, and the map can also be moved by scrolling with a " +
-                "\nmouse or clicking and dragging. Along the bottom of the map includes an emergency button (emergency) " +
-                "\nthat shows the nearest exit along with buttons to switch between floors of the hospital.");
+                "includes several features. Along the top are buttons that access staff login (login), " +
+                "a feedback/help/about screen (question mark), a filter for showing helpful icons on the map (filter), " +
+                "a tool to find the nearest type of location from the kiosk location (Find Nearest), " +
+                "and a button that brings the user to a screen to find directions from one place to another. " +
+                "Along the right side is a zoom feature, and the map can also be moved by scrolling with a " +
+                "mouse or clicking and dragging. Along the bottom of the map includes an emergency button (emergency) " +
+                "that shows the nearest exit along with buttons to switch between floors of the hospital.");
         this.descriptions.add(main);
         this.ImageMap.add("images/MainScreenPicture.png");
         CustomPair filter = new CustomPair("Filter on Main Menu", "When the filter button is clicked, it displays multiple buttons " +
-                "\nof popular areas (bathrooms, elevators, etc.) that people would " +
-                "\nwant to be able to see. Therefore, when this button is clicked (for " +
-                "\nexample in this picture bathroom was clicked), circles that grow and shrink " +
+                "of popular areas (bathrooms, elevators, etc.) that people would " +
+                "want to be able to see. Therefore, when this button is clicked (for " +
+                "example in this picture bathroom was clicked), circles that grow and shrink " +
                 "appear for a temporary amount of time at these areas to catch the users attention.");
         this.descriptions.add(filter);
         this.ImageMap.add("images/FilterScreenPictureThis.png");
         CustomPair FindNearest = new CustomPair("Find Nearest", "When the find nearest button is pressed, " +
-                "\nit displays multiple buttons of popular areas (bathrooms, elevators, etc.) that " +
+                "it displays multiple buttons of popular areas (bathrooms, elevators, etc.) that " +
                 "people would want to be able to see. Therefore, when this button is clicked (for " +
                 "example in this picture bathroom was clicked), a circle appears that grows and shrinks " +
-                "\nin size at the bathroom closest to the kiosk location.");
+                "in size at the bathroom closest to the kiosk location.");
         this.descriptions.add(FindNearest);
         this.ImageMap.add("images/FindNearestPictureThis.png");
         CustomPair Feedback = new CustomPair("Feedback Menu", "When the question mark button is pressed below the " +
-                "\nlogin on the main screen, it takes you to the pane seen in this image. Here, feedback " +
-                "\nwill be shown first and the user can tab between feedback, help, and about. Feedback " +
-                "\nallows the user to score the kiosk application from 0-5 and also provide additional " +
-                "\ncomments. The user can either press enter on the screen or press enter on the keyboard " +
-                "\nto save his/her feedback.");
+                "login on the main screen, it takes you to the pane seen in this image. Here, feedback " +
+                "will be shown first and the user can tab between feedback, help, and about. Feedback " +
+                "allows the user to score the kiosk application from 0-5 and also provide additional " +
+                "comments. The user can either press enter on the screen or press enter on the keyboard " +
+                "to save his/her feedback.");
         this.descriptions.add(Feedback);
         this.ImageMap.add("images/FeedbackPicture.png");
         CustomPair SearchText = new CustomPair("Searching by Text", "If the directions button is press on the main screen, " +
-                "\nthe user will then be brought to this screen to find directions from one place to another. " +
-                "\nThe user is still able to zoom / click and drag the map to navigate and can also see " +
-                "\nthe different floors of the map. Above, the user can click return to return to the main " +
-                "\nscreen of the map, and is also present with ways to search for places. The search method defaults to a text search, " +
-                "\nwhere the user can start typing to find a place or can click the arrow to see all of the available places. " +
-                "\nOnce the start node and end node are selected, the user can then click Go! to find the path.");
+                "the user will then be brought to this screen to find directions from one place to another. " +
+                "The user is still able to zoom / click and drag the map to navigate and can also see " +
+                "the different floors of the map. Above, the user can click return to return to the main " +
+                "screen of the map, and is also present with ways to search for places. The search method defaults to a text search, " +
+                "where the user can start typing to find a place or can click the arrow to see all of the available places. " +
+                "Once the start node and end node are selected, the user can then click Go! to find the path.");
         this.descriptions.add(SearchText);
         this.ImageMap.add("images/SearchByTextPicture.png");
         CustomPair SearchByType = new CustomPair("Searching by Type", "When the user clicks search by type, another search method is seen along the top. " +
-                "\nA user can select the type of place, then the floor number, and then the " +
-                "\navailable areas that match the type and floor number selected. " +
-                "\nOnce the start and end areas are selected, pressing Go! will find the path.");
+                "A user can select the type of place, then the floor number, and then the " +
+                "available areas that match the type and floor number selected. " +
+                "Once the start and end areas are selected, pressing Go! will find the path.");
         this.descriptions.add(SearchByType);
         this.ImageMap.add("images/SearchByTypePicture.png");
         CustomPair NodeToNode = new CustomPair("Directions Menu", "When Go! is clicked, a path is found based on the start and end areas." +
-                "\n A visual line with an animated arrow on the floor the path starts on is displayed, " +
-                "\nand leads the user to the destination if it is on the same floor or stairs/elevators " +
-                "\nthat the user has to take to get to the next floor the path goes to. Below where the " +
-                "\nfloors of the map were are now all the floors that the path visits which the user can " +
-                "\nclick through. Furthermore, towards the top left the user can click the text directions " +
-                "\nbutton which displays text directions related to the path. Lastly, towards the top right " +
-                "\nthe user can press the return button which reverses the path, making the starting area " +
-                "\nthe end area and vice versa.");
+                " A visual line with an animated arrow on the floor the path starts on is displayed, " +
+                "and leads the user to the destination if it is on the same floor or stairs/elevators " +
+                "that the user has to take to get to the next floor the path goes to. Below where the " +
+                "floors of the map were are now all the floors that the path visits which the user can " +
+                "click through. Furthermore, towards the top left the user can click the text directions " +
+                "button which displays text directions related to the path. Lastly, towards the top right " +
+                "the user can press the return button which reverses the path, making the starting area " +
+                "the end area and vice versa.");
         this.descriptions.add(NodeToNode);
         this.ImageMap.add("images/DirectionsPicture.png");
     }
