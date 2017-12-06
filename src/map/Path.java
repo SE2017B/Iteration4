9a0699fb6ebfce8d970278802c2b09d72db83fbe
Tvpatterson <tvpatterson@wpi.edu>
2017-12-06 00:50:00 -0500

    Much more improved text directions

--- src/map/Path.java
+++ src/map/Path.java
@@ -48,12 +48,12 @@ public class Path implements Comparable<Path> {
 
         if(path.get(0).getFloor().getNodeMapping() < path.get(1).getFloor().getNodeMapping()){
             prevElevator = true;
-            directions.add(0, "Go up " + path.get(0).getShortName());
+            directions.add(0, "Go up " + getBetterName(path.get(0)));
         } else if(path.get(0).getFloor().getNodeMapping() > path.get(1).getFloor().getNodeMapping()){
             prevElevator = true;
-            directions.add(0, "Go down " + path.get(0).getShortName());
+            directions.add(0, "Go down " + getBetterName(path.get(0)));
         } else {
-            directions.add(0, "Go straight from " + path.get(0).getShortName());
+            directions.add(0, "Go straight from " + getBetterName(path.get(0)));
         }
 
         for(int i = 1; i < path.size() - 1; i++){
@@ -70,17 +70,17 @@ public class Path implements Comparable<Path> {
 
 //            System.out.println(angle);
 //            System.out.println(prevElevator);
-            System.out.println("Node 1: " + path.get(i-1).getID());
-            System.out.println("Node 2: " + path.get(i).getID());
-            System.out.println("Node 3: " + path.get(i+1).getID() + "\n");
+            System.out.println("Node 1: " + path.get(i-1).getShortName());
+            System.out.println("Node 2: " + path.get(i).getShortName());
+            System.out.println("Node 3: " + path.get(i+1).getShortName() + "\n");
 
             if(Vector2_3Z != 0) {
                 if (path.get(i-1).getFloor().getNodeMapping() > path.get(i+1).getFloor().getNodeMapping() && !prevElevator) {
-                    directions.add("Go down " + path.get(i).getShortName());
+                    directions.add("Go down " + getBetterName(path.get(i)));
                     prevElevator = true;
                     continue;
                 } else if (path.get(i-1).getFloor().getNodeMapping() < path.get(i+1).getFloor().getNodeMapping() && !prevElevator) {
-                    directions.add("Go up " + path.get(i).getShortName());
+                    directions.add("Go up " + getBetterName(path.get(i)));
                     prevElevator = true;
                     continue;
                 }
@@ -89,48 +89,60 @@ public class Path implements Comparable<Path> {
                     path.get(i-1).getFloor().getNodeMapping() < path.get(i).getFloor().getNodeMapping()){
                     if(!prevElevator) directions.set(directions.size()-1, directions.get(directions.size()-1).substring(0, directions.get(directions.size()-1).length()-1).concat(path.get(i).getFloor().getDbMapping()));
                     else directions.set(directions.size()-1, directions.get(directions.size()-1).concat(" until floor " + path.get(i).getFloor().getDbMapping()));
-                    directions.add("Exit " + path.get(i).getShortName() + " and continue towards " + path.get(i+1).getShortName());
+                    directions.add("Exit " + getBetterName(path.get(i)) + " and continue towards " + getBetterName(path.get(i)));
                     prevElevator = false;
                     continue;
                 }
             }
 
             if(path.get(i).equals(path.get(i-1))){
-                directions.set(directions.size()-1, "Stop at " + path.get(i-1).getShortName());
+                directions.set(directions.size()-1, "Stop at " + getBetterName(path.get(i)));
                 prevStop = true;
             }
 
             if(angle >= -155 && angle <= -25){
                 if(straight) directions.add("Continue straight for " + path.get(lastStraight).getEuclidianDistance(path.get(i)) * pixelsToMeters + "m and take a left");
                 else if(prevElevator) directions.set(directions.size()-1, directions.get(directions.size()-1).concat(" until floor " + path.get(i).getFloor().getDbMapping()));
-                else if(prevStop) directions.add("Turn left from " + path.get(i).getShortName());
+                else if(prevStop) directions.add("Turn left from " + getBetterName(path.get(i)));
                 else {
-                    if(angle >= -40) directions.add("Take a slight left at " + path.get(i).getShortName());
-                    else if(angle <= -140) directions.add("Take a sharp left at " + path.get(i).getShortName());
-                    else directions.add("Take a left at " + path.get(i).getShortName());
+                    if(!path.get(i).getType().equals("HALL")) {
+                        if (angle >= -40) directions.add("Take a slight left at " + getBetterName(path.get(i)));
+                        else if (angle <= -140) directions.add("Take a sharp left at " + getBetterName(path.get(i)));
+                        else directions.add("Take a left at " + getBetterName(path.get(i)));
+                    } else {
+                        if (angle >= -40) directions.add("Take a slight left in the " + getBetterName(path.get(i)));
+                        else if (angle <= -140) directions.add("Take a sharp left in the " + getBetterName(path.get(i)));
+                        else directions.add("Take a left in the " + getBetterName(path.get(i)));
+                    }
                 }
             }
             else if(angle >= 25 && angle <= 155){
                 if(straight) directions.add("Continue straight for " + (int)(path.get(lastStraight).getEuclidianDistance(path.get(i)) * pixelsToMeters) + "m and take a right");
                 else if(prevElevator) directions.set(directions.size()-1, directions.get(directions.size()-1).concat(" until floor " + path.get(i).getFloor().getDbMapping()));
-                else if(prevStop) directions.add("Turn right from " + path.get(i).getShortName());
+                else if(prevStop) directions.add("Turn right from " + getBetterName(path.get(i)));
                 else {
-                    if(angle <= 40) directions.add("Take a slight right at " + path.get(i).getShortName());
-                    else if(angle >= 140) directions.add("Take a sharp right at " + path.get(i).getShortName());
-                    else directions.add("Take a right at " + path.get(i).getShortName());
+                    if(!path.get(i).getType().equals("HALL")) {
+                        if (angle <= 40) directions.add("Take a slight right at " + getBetterName(path.get(i)));
+                        else if (angle >= 140) directions.add("Take a sharp right at " + getBetterName(path.get(i)));
+                        else directions.add("Take a right at " + getBetterName(path.get(i)));
+                    } else {
+                        if (angle >= 40) directions.add("Take a slight right in the " + getBetterName(path.get(i)));
+                        else if (angle <= 140) directions.add("Take a sharp right in the " + getBetterName(path.get(i)));
+                        else directions.add("Take a right in the " + getBetterName(path.get(i)));
+                    }
                 }
             }
             else if (angle > -25 && angle < 25){
                 if(path.get(i).getType().equals("HALL")){
+                    if(!straight) lastStraight = i-1;
                     straight = true;
-                    lastStraight = i-1;
                     continue;
                 }
-                if(prevElevator || prevStop) directions.add("Go straight from " + path.get(i).getShortName());
-                else directions.add("Continue straight past " + path.get(i).getShortName());
+                if(prevElevator || prevStop) directions.add("Go straight from " + getBetterName(path.get(i)));
+                else directions.add("Continue straight past " + getBetterName(path.get(i)));
             }
             else {
-                directions.add("Turn around from " + path.get(i).getShortName());
+                directions.add("Turn around from " + getBetterName(path.get(i)));
             }
             prevElevator = false;
             prevStop = false;
@@ -141,7 +153,7 @@ public class Path implements Comparable<Path> {
                 path.get(path.size()-2).getFloor().getNodeMapping() < path.get(path.size()-1).getFloor().getNodeMapping()){
             directions.set(directions.size()-1, directions.get(directions.size()-1).substring(0, directions.get(directions.size()-1).length()-1).concat(path.get(path.size()-1).getFloor().getDbMapping()));
         }
-        directions.add("Stop at " + path.get(path.size()-1).getShortName());
+        directions.add("Stop at " + getFullType(path.get(path.size()-1)));
 
         //Eliminates multiple "go straights" in a row
 //        for(int i = 1; i<directions.size(); i++) {
@@ -151,6 +163,49 @@ public class Path implements Comparable<Path> {
 //            }
 //        }
         return directions;
+    }
+
+    private String getFullType(Node node){
+        String type = node.getType();
+        String string;
+        switch(type){
+            case "REST":
+                string = "your destination Restroom";
+                break;
+            case "ELEV":
+                string = "your destination Elevator";
+                break;
+            case "LABS":
+                string = node.getShortName();
+                break;
+            case "DEPT":
+                string = node.getShortName();
+                break;
+            case "INFO":
+                string = "the " + node.getLongName();
+                break;
+            case "STAI":
+                string = "your destination Stairwell";
+                break;
+            case "EXIT":
+                string = "your destination Exit";
+                break;
+            case "RETL":
+                string = node.getShortName();
+                break;
+            case "CONF":
+                string = node.getShortName();
+                break;
+            default: string = "";
+        }
+        return string;
+    }
+
+    private String getBetterName(Node node){
+        String string;
+        if(node.getType().equals("HALL")) string = "Hallway";
+        else string = node.getShortName();
+        return string;
     }
 
     //Retrieves the list of floors visited within a path
