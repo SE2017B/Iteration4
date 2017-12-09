e66693fa8d6ac67ab96579c91eae2f82aad0da78
Travis Norris <TravisYNorris@gmail.com>
2017-12-09 10:24:20 -0500

    Adding Help screen

---
+++ src/controllers/AnimatedHelpController.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXProgressBar;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import ui.AnimatedCircle;
+
+import java.util.ArrayList;
+
+public class AnimatedHelpController implements ControllableScreen{
+
+    ScreenController parent;
+
+    @Override
+    public void init(){
+
+    }
+
+    //reset everything to the start
+    @Override
+    public void onShow() {
+
+    }
+
+    @Override
+    public void setParentController(ScreenController parent) {
+        this.parent = parent;
+    }
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private JFXButton nextButton;
+
+    @FXML
+    private JFXButton previousButton;
+
+    @FXML
+    private Label helpTextLabel;
+
+    @FXML
+    private JFXButton returnButton;
+
+    private AnimatedCircle circle;
+
+    @FXML
+    private JFXProgressBar helpProgress;
+
+    @FXML
+    void nextPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void previousPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnPressed(ActionEvent event) {
+
+    }
+
+}
