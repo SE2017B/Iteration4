31e49321c3024cd7e3c71c99f784598a2e29b2d6
Tvpatterson <tvpatterson@wpi.edu>
2017-12-10 18:57:22 -0500

    Undo backend 95%

--- src/ui/EdgeCheckBox.java
+++ src/ui/EdgeCheckBox.java
@@ -3,23 +3,29 @@ package ui;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import map.Edge;
+import map.HospitalMap;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class EdgeCheckBox extends Line {
     private Edge edge;
+    private HospitalMap map;
     private boolean selected;
 
     public EdgeCheckBox(Edge edge) {
         super();
+        map = HospitalMap.getMap();
         this.edge = edge;
         selected = false;
-        int startX = edge.getNodeOne().getX();
-        int startY = edge.getNodeOne().getY();
+        int startX = map.getNodeMap().stream().filter(n1->n1.equals(edge.getNodeOne())).collect(Collectors.toList()).get(0).getX();
+        int startY = map.getNodeMap().stream().filter(n1->n1.equals(edge.getNodeOne())).collect(Collectors.toList()).get(0).getY();
         this.setLayoutX(startX);
         this.setLayoutY(startY);
         this.setStartX(0);
         this.setStartY(0);
-        this.setEndX((edge.getNodeTwo().getX() - startX));
-        this.setEndY((edge.getNodeTwo().getY() - startY));
+        this.setEndX((map.getNodeMap().stream().filter(n1->n1.equals(edge.getNodeTwo())).collect(Collectors.toList()).get(0).getX() - startX));
+        this.setEndY((map.getNodeMap().stream().filter(n1->n1.equals(edge.getNodeTwo())).collect(Collectors.toList()).get(0).getY() - startY));
         this.setStrokeWidth(8);
         this.setVisible(false);
     }
